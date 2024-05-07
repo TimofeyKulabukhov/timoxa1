86358d97f9ba5e62ffb36c836b141f787109af77
TimofeyKulabukhov <167567158+TimofeyKulabukhov@users.noreply.github.com>
2024-05-07 13:15:15 +0300

    update port

--- src/app.py
+++ src/app.py
@@ -120,5 +120,6 @@ if __name__ == '__main__':
     app.run(debug=True)
     
 if __name__ =='__main__':
-    app.run(host="0.0.0.0")
+     app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
     
