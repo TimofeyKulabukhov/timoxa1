dd55f889dabe2a4b1ad14a4fcca05a9d6ec332e8
TimofeyKulabukhov <167567158+TimofeyKulabukhov@users.noreply.github.com>
2024-05-07 16:14:57 +0300

    update port

--- src/app.py
+++ src/app.py
@@ -119,6 +119,8 @@ if __name__ == '__main__':
     app.secret_key = os.urandom(24)
     app.run(debug=True)
     
+    if __name__ == '__main__':
+        app.run ("0.0.0.0", port=10000)
  
     
     
